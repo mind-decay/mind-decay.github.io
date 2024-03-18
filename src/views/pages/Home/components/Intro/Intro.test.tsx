d58351b01465991595813e7f51b03ece28490212
mind-decay <adamdok53@gmail.com>
2024-03-18 12:11:42 +0700

    fix intro section tests

--- src/views/pages/Home/components/Intro/Intro.test.tsx
+++ src/views/pages/Home/components/Intro/Intro.test.tsx
@@ -2,12 +2,15 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ThemeProvider } from 'context/ThemeContext/ThemeContext';
 import { Intro } from './Intro';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders Intro section', () => {
   const { container } = render(
-    <ThemeProvider>
-      <Intro />
-    </ThemeProvider>,
+    <BrowserRouter>
+      <ThemeProvider>
+        <Intro />
+      </ThemeProvider>
+    </BrowserRouter>,
   );
 
   expect(container).toMatchSnapshot();
